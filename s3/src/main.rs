57b6c61623b1f551ea1a927d4b1dbf953da4a774
hiroto0927 <hiroto.0927.123@gmail.com>
2025-05-27 15:16:44 +0000

    S3の署名付きURLにRUSTでアップロードできることを確認

--- s3/src/main.rs
+++ s3/src/main.rs
@@ -1,21 +1,23 @@
-use std::fs::File;
-use std::io::Read;
-
-async fn sub(file: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
-    let url = "https://etl-test-higuchi.s3.ap-northeast-1.amazonaws.com/rust-upload/test.txt?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA4QNCPZ6H5EBMU6XN%2F20250526%2Fap-northeast-1%2Fs3%2Faws4_request&X-Amz-Date=20250526T105252Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=fca268dc5a16e6a7b8e3ef3a2eabd669c6d1f0fdd50736444a520712de9282a5";
-    let client = reqwest::Client::new();
-    let response = client.put(url)
-    println!("{:?}", response);
-    Ok(())
-}
+use reqwest::Body;
+use tokio::{fs::File, io::AsyncReadExt};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // sub().await?;
 
-    let mut file = File::open("./test.txt")?;
-    
-    // println!("{:?}", buf);
+    let tmp_url = "";
+    let mut file = File::open("./test.txt").await?;
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf).await?;
+
+    let client = reqwest::Client::new();
+    let res = client.put(tmp_url).body(Body::from(buf)).send().await?;
+
+    if res.status().is_success() {
+        println!("File uploaded successfully.");
+    } else {
+        println!("Failed to upload file: {}", res.status());
+    }
 
     Ok(())
 }
